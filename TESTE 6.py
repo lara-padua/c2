c1145742732cdff2a325cfe70b9d7c15b3e06819
Lara Pádua <larapadualugao@usp.br>
2025-05-24 14:26:01 -0300

    teste 6

---
+++ TESTE 6.py
@@ -0,0 +1,12 @@
+print("Escolha a sua opção preferida do bandeco:")
+x = int (input("1-Fricassê de frango\n2-Carne de porco\n 3-Bife de hambúrguer com macarrão\n 4-Strogonofe de carne\n 5-Frango assado\n"))
+if (x==1):
+    print("Seu prato favorito é o fricassê de frango")
+if (x==2):
+    print("Seu prato favorito é a carne de porco")
+if (x==3):
+    print("Seu prato favorito é o bife de hambúrguer com macarrão")
+if (x==4):
+    print("Seu prato favorito é o strogonofe de carne")
+if (x==5):
+    print("Seu prato favorito é o frango assadoo")
