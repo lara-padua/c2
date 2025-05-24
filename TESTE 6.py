02c9cd628b8b82172dd7029661988c9bd2e618f1
Lara Pádua <larapadualugao@usp.br>
2025-05-24 14:45:58 -0300

    concertei o 6 e adicionei o 7

--- TESTE 6.py
+++ TESTE 6.py
@@ -2,11 +2,11 @@ print("Escolha a sua opção preferida do bandeco:")
 x = int (input("1-Fricassê de frango\n2-Carne de porco\n 3-Bife de hambúrguer com macarrão\n 4-Strogonofe de carne\n 5-Frango assado\n"))
 if (x==1):
     print("Seu prato favorito é o fricassê de frango")
-if (x==2):
+elif (x==2):
     print("Seu prato favorito é a carne de porco")
-if (x==3):
+elif (x==3):
     print("Seu prato favorito é o bife de hambúrguer com macarrão")
-if (x==4):
+elif (x==4):
     print("Seu prato favorito é o strogonofe de carne")
-if (x==5):
+else:
     print("Seu prato favorito é o frango assadoo")
---
+++ TESTE 7.py
@@ -0,0 +1,7 @@
+print("Vamos fazer uma comparação entre dois números!")
+x = float(input("Escolha o primeiro número:\n"))
+y = float(input("Agora escolha o segundo número:\n"))
+if x>y:
+    print(x,"é maior que",y,".")
+else:
+    print(y,"é maior que",x,".")
